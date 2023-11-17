ff00bb72b1ca2776d114467e05034289424929e7
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2023-11-17 13:21:53 +0530

    Add enemy types & movements

---
+++ enemy.js
@@ -0,0 +1,102 @@
+class Enemy {
+  constructor() {
+    this.frameX = 0;
+    this.frameY = 0;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+    this.markedForRemoval = false;
+  }
+  update(deltaTime) {
+    this.x -= this.speedX + this.game.speed;
+    this.y -= this.speedY;
+
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX >= this.maxFrames) this.frameX = 0;
+      else this.frameX += 1;
+    } else this.frameTimer += deltaTime;
+
+    if (this.x + this.width < 0) this.markedForRemoval = true;
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+export class FlyingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.maxFrames = 4;
+    this.speedX = 2;
+    this.speedY = 0;
+    this.width = 60;
+    this.height = 44;
+    this.image = fly;
+    this.angle = 0;
+    this.va = Math.random() * 0.2 + 0.1;
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+    this.angle += this.va;
+    this.y += Math.sin(this.angle);
+  }
+}
+
+export class GroundEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 87;
+    this.x = this.game.width;
+    this.y = this.game.height - this.game.gameMargin - this.height;
+    this.maxFrames = 1;
+    this.speedX = 0;
+    this.speedY = 0;
+
+    this.image = plant;
+  }
+}
+
+export class ClimbingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.x = this.game.width;
+    this.y = Math.random() * 0.5 * this.game.height;
+    this.maxFrames = 5;
+    this.image = big_spider;
+    this.width = 120;
+    this.height = 144;
+    this.speedX = 0;
+    this.speedY = Math.random() > 0.5 ? 1 : -1;
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+
+    if (this.y > this.game.height - this.game.gameMargin - this.height)
+      this.speedY *= -1;
+    if (this.y < -this.height) this.markedForRemoval = true;
+  }
+  draw(context) {
+    super.draw(context);
+    context.beginPath();
+    context.moveTo(this.x + this.width * 0.5, 0);
+    context.lineTo(this.x + this.width * 0.5, this.y + 50);
+    context.stroke();
+  }
+}
--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemy.js";
 
 window.addEventListener("load", () => {
   const canvas = document.getElementById("canvas");
@@ -14,20 +15,43 @@ window.addEventListener("load", () => {
     constructor(width, height) {
       this.width = width;
       this.height = height;
-      this.gameMargin = 75;
+      this.gameMargin = 80;
       this.speed = 0;
       this.maxSpeed = 3;
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemySponInterval = 1000;
     }
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+
+      if (this.enemyTimer > this.enemySponInterval) {
+        this.enemyTimer = 0;
+        this.addEnemy();
+      } else this.enemyTimer += deltaTime;
+
+      this.enemies.forEach((enemy) => {
+        enemy.update(deltaTime);
+        if (enemy.markedForRemoval)
+          this.enemies.splice(this.enemies.indexOf(enemy), 1);
+      });
     }
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+
+      this.enemies.forEach((enemy) => enemy.draw(context));
+    }
+    addEnemy() {
+      if (this.speed > 0 && Math.random() > 0.5)
+        this.enemies.push(new GroundEnemy(this));
+      else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+      this.enemies.push(new FlyingEnemy(this));
+      console.log(this.enemies);
     }
   }
 
